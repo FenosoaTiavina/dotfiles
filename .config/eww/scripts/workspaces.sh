bf0cb7a504733b03a3137d924fe980195a20edf4
FenosoaTiavina <fenosoa.t.r@fenosoa.com>
2025-06-18 09:27:21 +0300

    Simpler using css

--- .config/eww/scripts/workspaces.sh
+++ .config/eww/scripts/workspaces.sh
@@ -1,3 +1,3 @@
 socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
-  hyprctl workspaces -j | jq -c 'sort_by(.id) | map(select(.id>0))'
+    hyprctl workspaces -j | jq -c 'sort_by(.id) | map(select(.id>0))'
 done
