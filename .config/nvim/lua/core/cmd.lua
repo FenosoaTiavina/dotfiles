49b134bcf730763e16d0f3cbc81d7c03aa388712
FenosoaTiavina <fenosoa.t.r@fenosoa.com>
2025-07-04 09:53:30 +0300

    hypr

--- .config/nvim/lua/core/cmd.lua
+++ .config/nvim/lua/core/cmd.lua
@@ -8,3 +8,18 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     end
   end
 })
+
+
+-- Fuzzy finder
+vim.api.nvim_create_autocmd("User", {
+  pattern = "TelescopeFindPre",
+  callback = function()
+    vim.opt_local.winborder = "none"
+    vim.api.nvim_create_autocmd("WinLeave", {
+      once = true,
+      callback = function()
+        vim.opt_local.winborder = "rounded"
+      end,
+    })
+  end,
+})
