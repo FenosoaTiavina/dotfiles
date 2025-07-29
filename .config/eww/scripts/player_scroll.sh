c8141296207d00063f1877ebe3049f7c080d769b
FenosoaTiavina <fenosoa.t.r@fenosoa.com>
2025-07-29 17:38:48 +0300

    volumes

--- .config/eww/scripts/player_scroll.sh
+++ .config/eww/scripts/player_scroll.sh
@@ -2,9 +2,12 @@
 
 skip_amount=0.1
 
+notify-send "$3"
 
 if [ "$2" == "up" ] ; then
   playerctl -i firefox $1 $skip_amount+
 elif [ "$2" == "down" ]; then
-  playerctl -i firefox $1 $skip_amount-
+  if [ $3 > 0 ] ; then
+    playerctl -i firefox $1 $skip_amount-
+  fi
 fi
