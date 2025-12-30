77b2badebee4215f9428918e99a88a3190e88194
FenosoaTiavina <fenosoa.t.r@fenosoa.com>
2025-12-30 23:32:22 +0300

    vicinae

--- .config/nvim/lua/core/lsp/zls.lua
+++ .config/nvim/lua/core/lsp/zls.lua
@@ -2,21 +2,21 @@ local on_attach = require("core.lsp.utils").on_attach
 local capabilities = require("core.lsp.utils").capabilities
 
 return {
-  cmd                 = {
-    "zls",
-  },
-  filetypes           = {
-    "zig",
-  },
+    cmd                 = {
+        "zls",
+    },
+    filetypes           = {
+        "zig",
+    },
 
-  on_attach           = on_attach,
-  capabilities        = capabilities,
+    on_attach           = on_attach,
+    capabilities        = capabilities,
 
-  root_markers        = {
-    ".git",
-    "build.zig",
-    "build.zig.zon",
-  },
-  single_file_support = true,
-  log_level           = vim.lsp.protocol.MessageType.Warning,
+    root_markers        = {
+        ".git",
+        "build.zig",
+        "build.zig.zon",
+    },
+    single_file_support = true,
+    log_level           = vim.lsp.protocol.MessageType.Warning,
 }
