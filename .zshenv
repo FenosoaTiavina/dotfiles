544ccd080cd793ceacc765896b7863c39a5365cf
FenosoaTiavina <fenosoa.t.r@fenosoa.com>
2026-02-13 22:24:51 +0300

    move shell env

--- .zshenv
+++ .zshenv
@@ -2,4 +2,10 @@ if [ -n "${ZSH_VERSION-}" ]; then
   : ${ZDOTDIR:=~/.config/zsh}
 fi
 
-source ~/devenv
+source ~/devenv.sh
+
+# Added by Radicle.
+export PATH="$PATH:/home/fenosoa/.radicle/bin"
+
+source $HOME/dev/lib/VulkanSDK/setup-env.sh
+
---
+++ devenv.sh
@@ -0,0 +1,9 @@
+# local bin
+export PATH="$PATH:/home/fenosoa/.local/bin"
+# local scripts
+export PATH="$PATH:/home/fenosoa/.local/bin/scripts"
+
+export EDITOR=/usr/bin/nvim
+export MANPAGER="nvim +Man! "
+
+source ~/Music/dab-downloader.zsh
